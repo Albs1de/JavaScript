47c756436282d4eabf85954b4368ead19f7804c1
Driton <driton.koskovik@icloud.com>
2022-10-11 10:51:56 +0200

    Fixed create Card

--- Library Project/js/script.js
+++ Library Project/js/script.js
@@ -15,8 +15,9 @@ let newAuthor = document.getElementById("nAuthor");
 let newPages = document.getElementById("nPages");
 
 //Add a function that take users input and store the new book objects into an array.
-function addBookToLibrary(newTitle, newAuthor, newPages)
+function addBookToLibrary()
 {
+   
    const newBook = new Book(newTitle, newAuthor, newPages);
     myLibrary.push(newBook);
 
@@ -41,15 +42,17 @@ function loopArray()
 
 loopArray();
 loopArray();
-//Add a "new book"-Button that brings up a form allowing users to input the details for the new book and whether its been read etc.
+//* TODO Add a "new book"-Button that brings up a form allowing users to input the details for the new book and whether its been read etc.
 
-//Add a button that remove the book from the library
+//* TODO Add a button that remove the book from the library
 
+
+// *show div Block
 
 //Show and hide Div Block
 function hideCard()
 {
-    let hideDiv = document.getElementById("newDiv");
+    let hideDiv = document.getElementById("newBaseDiv");
     if (hideDiv.style.display === "none")
     {
         hideDiv.style.display = "block";
@@ -65,20 +68,27 @@ addNewBook.addEventListener('click', createBook);
 //Create new Div block with new book properties
 function createBook()
 {
-    
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("flex-direction", "column");
-    newDiv.setAttribute('background-color', 'black');
+    //create new Div for the elements
+    const newBaseDiv = document.createElement("div");
+    const newDivItems = document.createElement("div");
+    const currentDiv = document.getElementById("newBook");
+    //set attributes from newDivItems
+    currentDiv.style.flexDirection = "row";
     //create Elements
-    const newTitles = document.createTextNode(newTitle.value);
-    const newAuthors = document.createTextNode(newAuthor.value);
+    const newTitles = document.createTextNode(newTitle.value + " ");
+    const newAuthors = document.createTextNode(newAuthor.value + " ");
     const newPage = document.createTextNode(newPages.value);
 
-    // add elements to newDiv
-    newDiv.appendChild(newTitles);
-    newDiv.appendChild(newAuthors);
-    newDiv.appendChild(newPage);
+    // add elements to newBaseDiv
+    newDivItems.appendChild(newTitles);
+    
+    newDivItems.appendChild(newAuthors);
+    
+    newDivItems.appendChild(newPage);
+
+    currentDiv.appendChild(newDivItems);
+    addBookToLibrary();
     // add the newly created element + content to the DOM
-    const currentDiv = document.getElementById("newBook");
-   document.body.insertBefore(newDiv, currentDiv);
+    
+   document.body.insertBefore(newDivItems, currentDiv);
 }
